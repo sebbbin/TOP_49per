3e9fc67fa40e6a6fa2077faff66227c7c9ff5fa4
asdf1019 <jiwon3157@naver.com>
2023-11-19 22:11:09 +0900

    navigationbar timer추가

--- app/src/main/java/com/example/tomate/MainActivity.java
+++ app/src/main/java/com/example/tomate/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.tomate;
 
+import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -35,13 +39,27 @@ public class MainActivity extends AppCompatActivity {
             public void onNavigationItemReselected(@NonNull MenuItem item) {
                 switch (item.getItemId()) {
                     case R.id.navigation_record:
-                        getSupportFragmentManager().beginTransaction().replace(R.id.main_frame, new RestFragment()).commit();
+                        getSupportFragmentManager().beginTransaction().replace(R.id.main_frame, new StudyrecordFragment()).commit();
                         break;
                     case R.id.navigation_ranking:
                         getSupportFragmentManager().beginTransaction().replace(R.id.main_frame, new RankingFragment()).commit();
                         break;
                 }
                 return;
+            }
+        });
+
+        ImageButton timerButton = findViewById(R.id.timer_button);
+        Drawable drawable = getResources().getDrawable(R.drawable.tomato_3d);
+        timerButton.setImageDrawable(drawable);
+
+        timerButton.setAdjustViewBounds(true);
+        timerButton.setScaleType(ImageButton.ScaleType.FIT_CENTER);
+        timerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, TimerActivity.class);
+                startActivity(intent);
             }
         });
     }
