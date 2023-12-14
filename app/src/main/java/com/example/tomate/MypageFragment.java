a47bbea3403054315a996a9d2f8d2c4c81aff7d5
minheeLee <87891501+MoOo2mini@users.noreply.github.com>
2023-12-15 03:49:17 +0900

    modify: 동적으로 시작날짜 count

--- app/src/main/java/com/example/tomate/MypageFragment.java
+++ app/src/main/java/com/example/tomate/MypageFragment.java
@@ -1,6 +1,8 @@
 package com.example.tomate;
 
 import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -24,13 +26,18 @@ import com.kakao.sdk.user.UserApiClient;
 
 import org.jetbrains.annotations.Nullable;
 
+import java.text.SimpleDateFormat;
 import java.time.Duration;
+import java.time.LocalDate;
 import java.time.LocalTime;
+import java.time.temporal.ChronoUnit;
 import java.util.Collections;
+import java.util.Date;
 import java.util.Iterator;
 
 import kotlin.Unit;
 import kotlin.jvm.functions.Function1;
+import kotlin.jvm.functions.Function2;
 
 public class MypageFragment extends Fragment {
     ViewGroup rootView;
@@ -38,11 +45,12 @@ public class MypageFragment extends Fragment {
     String tierStr;
     String startDate;
     String userId;
+    MainActivity mainActivity;
 
     @Override
     public void onAttach(@NonNull Context context) {
         super.onAttach(context);
-        MainActivity mainActivity = (MainActivity)getActivity();
+        mainActivity = (MainActivity)getActivity();
         userId = mainActivity.userId;
     }
 
@@ -53,24 +61,32 @@ public class MypageFragment extends Fragment {
         getUserData();
 
         TextView signoutTv = rootView.findViewById(R.id.fragment_mypage_member_out_tv);
+
         signoutTv.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                SharedPreferences sharedPref = getActivity().getSharedPreferences("MyPrefs", Context.MODE_PRIVATE);
+
+                // "userId" 키로 저장된 값을 가져옵니다. 기본값으로 "" (빈 문자열)을 사용합니다.
+                String userId = sharedPref.getString("userId", "");
+                Log.d("TAG", "UserId: " + userId);
+
                 DatabaseReference mDatabase = FirebaseDatabase.getInstance().getReference("User");
-                DatabaseReference userRef = mDatabase.child("user6");
+                DatabaseReference userRef = mDatabase.child(userId);
 
                 // user1 데이터 삭제
                 userRef.removeValue().addOnSuccessListener(new OnSuccessListener<Void>() {
                     @Override
                     public void onSuccess(Void aVoid) {
                         // 데이터 삭제 성공 시 처리
-                        System.out.println("User1 data successfully deleted.");
+                        System.out.println(userId + "data successfully deleted.");
+                        mainActivity.logoutOrSignout();
                     }
                 }).addOnFailureListener(new OnFailureListener() {
                     @Override
                     public void onFailure(@NonNull Exception e) {
                         // 데이터 삭제 실패 시 처리
-                        System.out.println("Error deleting User1 data: " + e.getMessage());
+                        System.out.println("Error deleting user data: " + e.getMessage());
                     }
                 });
 
@@ -78,27 +94,35 @@ public class MypageFragment extends Fragment {
         });
 
         TextView logoutTv = rootView.findViewById(R.id.fragment_mypage_logout_tv);
+
         logoutTv.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                // 로그아읏 버튼
-                logoutTv.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-                        UserApiClient.getInstance().logout(new Function1<Throwable, Unit>() {
-                            @Override
-                            public Unit invoke(Throwable throwable) {
-//                                updateKakaoLoginUi();
-                                return null;
-                            }
-                        });
-                    }
-                });
+                // SharedPreferences 객체를 가져옵니다. "MyPrefs"는 SharedPreferences 파일의 이름입니다.
+                SharedPreferences sharedPref = getActivity().getSharedPreferences("MyPrefs", Context.MODE_PRIVATE);
+
+                // SharedPreferences.Editor 객체를 사용하여 데이터를 수정합니다.
+                SharedPreferences.Editor editor = sharedPref.edit();
+
+                // "userId" 키에 해당하는 데이터를 제거합니다.
+                editor.remove("userId");
+
+                // 변경사항을 커밋합니다.
+                editor.apply();
+                mainActivity.logoutOrSignout();
             }
         });
         return rootView;
     }
+
     private void getUserData() {
+        // SharedPreferences 객체를 가져옵니다. "MyPrefs"는 SharedPreferences 파일의 이름입니다.
+        SharedPreferences sharedPref = getActivity().getSharedPreferences("MyPrefs", Context.MODE_PRIVATE);
+
+        // "userId" 키로 저장된 값을 가져옵니다. 기본값으로 "" (빈 문자열)을 사용합니다.
+        String userId = sharedPref.getString("userId", "");
+        Log.d("TAG", "UserId: " + userId);
+
         DatabaseReference databaseRef = FirebaseDatabase.getInstance().getReference();
         // userId에 해당하는 user의 total_cnt를 조회
         databaseRef.child("User").orderByChild("userId").equalTo(Long.parseLong(userId))
@@ -114,6 +138,8 @@ public class MypageFragment extends Fragment {
                                 tierTv.setText(tierStr);
                                 TextView userNameTv = rootView.findViewById(R.id.fragment_mypage_name_tv);
                                 userNameTv.setText(userName);
+                                TextView startDateTv = rootView.findViewById(R.id.fragment_mypage_day_count_tv);
+                                startDateTv.setText(dateDiff(user.getStartDate())+"일째");
                                 ImageView tierIv = rootView.findViewById(R.id.fragment_mypage_tear_iv);
                                 if (tierStr.equals("토마토마스터")) {
                                     tierIv.setImageResource(R.drawable.tomato_master);
@@ -137,5 +163,24 @@ public class MypageFragment extends Fragment {
                         Log.d("onCancelled", "onCancelled");
                     }
                 });
+    }
+    private String getTodayDate() {
+        Date date = new Date();
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+        String dateString = formatter.format(date);
+        System.out.println("오늘 날짜: " + dateString);
+        return dateString;
+    }
+
+    private String dateDiff(String startDate) {
+        String currentDate = getTodayDate();
+
+        LocalDate start = LocalDate.parse(startDate);
+        LocalDate current = LocalDate.parse(currentDate);
+
+        // 두 날짜 사이의 차이를 일(days) 단위로 계산
+        long daysBetween = ChronoUnit.DAYS.between(start, current) + 1;
+
+        return String.valueOf(daysBetween);
     }
 }
