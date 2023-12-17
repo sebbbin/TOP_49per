7b14f542ed4918183098a3526a4253ee13e7975a
minheeLee <87891501+MoOo2mini@users.noreply.github.com>
2023-12-17 14:44:58 +0900

    fix : bug

--- app/src/main/java/com/example/tomate/MainActivity.java
+++ app/src/main/java/com/example/tomate/MainActivity.java
@@ -46,7 +46,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onDestroy() {
         super.onDestroy();
         Log.d("onDestroy", "main");
-        logoutOrSignout();
     }
 
     @Override
